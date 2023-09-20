fa24c7747a535ab041ba8222db98c388bca41d8e
xramyuntoo <143625055+xramyuntoo@users.noreply.github.com>
2023-09-20 10:49:08 -0400

    Add background color change on mouseover

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ container.id = "container";
 for (let i = 0; i < 16; i++) {
     let newDiv = document.createElement("div");
     newDiv.className = "sketchBox";
+    
+    newDiv.addEventListener("mouseover", (event) => {
+        newDiv.style.backgroundColor = "black";
+    });
     container.appendChild(newDiv);
 }
+
+
+
+
 
 body.appendChild(container);
